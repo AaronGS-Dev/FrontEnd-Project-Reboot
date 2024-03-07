469d5fa9afda63ecb6c4e042671bd1e43dc81727
AaronGS-Dev <147653788+AaronGS-Dev@users.noreply.github.com>
2024-03-07 11:24:13 +0000

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',   
+    baseURL: 'https://reboot-final-project.onrender.com/api',   
   
   });
   export default api
